e39e5b9d5e22af63645281e82537af75d666ee32
mehulthakkar <mehulthakkar.linkedin@gmail.com>
2021-12-10 22:05:26 -0600

    Added Cassandra configuration.

---
+++ src/main/java/config/CassandraConfiguration.java
@@ -0,0 +1,44 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.SchemaAction;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableCassandraRepositories
+public class CassandraConfiguration extends AbstractCassandraConfiguration {
+
+    @Value("${cassandra.keyspace.placeholder}")
+    private String cassandraKeySpace;
+
+    @Value("${cassandra.contactpoints:placeholder}")
+    private String cassandraContactPoints;
+
+    @Value("${cassandra.port:0000}")
+    private int port;
+
+    @Value("${cassandra.datacenter:placeholder")
+    private String datacenter;
+
+    @Override
+    public int getPort() {
+        return port;
+    }
+
+    @Override
+    public String getKeyspaceName() {
+        return cassandraKeySpace;
+    }
+
+    @Override
+    public String getContactPoints() {
+        return cassandraContactPoints;
+    }
+
+    @Override
+    public SchemaAction getSchemaAction() {
+        return SchemaAction.CREATE_IF_NOT_EXISTS;
+    }
+}
--- src/main/java/config/TemporalConfig.java
+++ src/main/java/config/TemporalConfig.java
@@ -6,7 +6,8 @@ import io.temporal.client.WorkflowClientOptions;
 import io.temporal.serviceclient.WorkflowServiceStubs;
 import io.temporal.serviceclient.WorkflowServiceStubsOptions;
 import io.temporal.worker.WorkerFactory;
-import lombok.Value;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
